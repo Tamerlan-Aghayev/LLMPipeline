fb431167ac2cf02a68d61ff7b5d4ee98f05be0e8
TomiDev004 <tomiqaqa2004@gmail.com>
2024-08-18 00:17:52 +0400

    added semantic search for columns as well, for better prompt generation

--- src/semantic_search/semantic_matcher.py
+++ src/semantic_search/semantic_matcher.py
@@ -7,20 +7,31 @@ class SemanticMatcher:
 
     def semantic_search(self, query, all_schemas):
         query_embedding = self.model.encode(query, convert_to_tensor=True)
-        
+
         table_embeddings = []
         table_info = []
+        column_embeddings = []
+        column_info = []
 
         for schema_name, schema in all_schemas.items():
             for table in schema['tables']:
+                # Encode table
                 table_embeddings.append(self.model.encode(f"{schema_name} {table['name']}", convert_to_tensor=True))
                 table_info.append((schema_name, table))
 
+                # Encode columns
+                for column in table['columns']:
+                    column_embeddings.append(self.model.encode(f"{schema_name} {table['name']} {column['name']}", convert_to_tensor=True))
+                    column_info.append((schema_name, table, column))
+
         table_embeddings = torch.stack(table_embeddings)
+        column_embeddings = torch.stack(column_embeddings)
 
         table_similarities = util.pytorch_cos_sim(query_embedding, table_embeddings)[0]
+        column_similarities = util.pytorch_cos_sim(query_embedding, column_embeddings)[0]
 
         top_tables = torch.topk(table_similarities, k=min(3, len(table_info)))
+        top_columns = torch.topk(column_similarities, k=min(5, len(column_info)))
 
         result = {
             "tables": [
@@ -29,7 +40,15 @@ class SemanticMatcher:
                     "table": table_info[i][1],
                     "score": score.item()
                 } for i, score in zip(top_tables.indices, top_tables.values)
+            ],
+            "columns": [
+                {
+                    "schema": column_info[i][0],
+                    "table": column_info[i][1],
+                    "column": column_info[i][2],
+                    "score": score.item()
+                } for i, score in zip(top_columns.indices, top_columns.values)
             ]
         }
-        
+
         return result
